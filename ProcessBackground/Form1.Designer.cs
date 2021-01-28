21f524143bb81a760dd70157e91e56723e39ff5e
Danny Pruitt <dannypruitt@gmail.com>
2021-01-27 19:59:04 -0500

    Add project files.

---
+++ ProcessBackground/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ProcessBackground
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRunProcessAsync = new System.Windows.Forms.Button();
+            this.textOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonRunProcessAsync
+            // 
+            this.buttonRunProcessAsync.Location = new System.Drawing.Point(81, 23);
+            this.buttonRunProcessAsync.Name = "buttonRunProcessAsync";
+            this.buttonRunProcessAsync.Size = new System.Drawing.Size(133, 86);
+            this.buttonRunProcessAsync.TabIndex = 0;
+            this.buttonRunProcessAsync.Text = "Run Process Async";
+            this.buttonRunProcessAsync.UseVisualStyleBackColor = true;
+            this.buttonRunProcessAsync.Click += new System.EventHandler(this.RunProcessAsync_Click);
+            // 
+            // textOutput
+            // 
+            this.textOutput.Location = new System.Drawing.Point(81, 139);
+            this.textOutput.Multiline = true;
+            this.textOutput.Name = "textOutput";
+            this.textOutput.Size = new System.Drawing.Size(596, 255);
+            this.textOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textOutput);
+            this.Controls.Add(this.buttonRunProcessAsync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRunProcessAsync;
+        private System.Windows.Forms.TextBox textOutput;
+    }
+}
+
---
+++ ProcessBackground/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ProcessBackground
+{
+    public partial class Form1 : Form
+    {
+        public delegate void SafeCallDelegate(string text);
+
+        public SafeCallDelegate myDelegate;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            myDelegate = new SafeCallDelegate(WriteTextThreadSafe);
+        }
+
+        private void RunProcessAsync_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+
+                var worker = new BackgroundWorker();
+                worker.WorkerReportsProgress = false;
+                worker.WorkerSupportsCancellation = false;
+                worker.DoWork += worker_DoWork;
+                worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+                worker.RunWorkerAsync();
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            this.RunProcess();
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.WriteTextThreadSafe("process complete");
+        }
+
+        private void RunProcess()
+        {
+            using (Process process = new Process())
+            {
+                var processInfo = new ProcessStartInfo("cmd.exe", $"/c timeout 5 & echo 1 & echo 2 &  timeout 5 & echo 3 & echo 4 &  timeout 6");
+                processInfo.CreateNoWindow = true;
+                processInfo.UseShellExecute = false;
+                processInfo.RedirectStandardError = true;
+                processInfo.RedirectStandardOutput = true;
+
+                process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+                process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+
+                process.StartInfo = processInfo;
+                process.Start();
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit();
+
+                process.Close();
+            }
+        }
+
+        void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+
+            this.WriteTextThreadSafe(outLine.Data);
+        }
+
+        private void WriteTextThreadSafe(string text)
+        {
+            this.BeginInvoke(
+                ((MethodInvoker)delegate
+            {
+                this.textOutput.Text += text + Environment.NewLine;
+            }));
+        }
+    }
+}
